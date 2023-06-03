4d222b40a54bfcb5076b61a445014e671d1c9377
Aillet <arthur.aillet@epitech.eu>
2023-06-03 20:35:32 +0200

    feat(Camera): movement handled and new method created

--- src/camera.rs
+++ src/camera.rs
@@ -1,9 +1,9 @@
 use glam::Vec3A as Vector;
 
-struct Camera {
-    position: Vector,
-    pitch: f32,
-    yaw: f32,
+pub struct Camera {
+    pub position: Vector,
+    pub pitch: f32,
+    pub yaw: f32,
 }
 
     fn pitch_yaw_to_direction(pitch: f32, yaw: f32) -> Vector {
@@ -15,6 +15,14 @@ struct Camera {
     }
 
 impl Camera {
+    pub fn new() -> Camera {
+        Camera {
+            position: Vector::new(0., 0., 0.),
+            pitch: 0.0,
+            yaw: std::f32::consts::PI * 0.5,
+        }
+    }
+
     fn direction(&self) -> Vector {
         pitch_yaw_to_direction(self.pitch, self.yaw)
     }
@@ -31,7 +39,24 @@ impl Camera {
         )
     }
 
-    // The camera's "view" matrix.
+    pub fn move_forward(&mut self, distance: f32) {
+        self.position += self.direction() * distance;
+    }
+
+    pub fn move_left(&mut self, distance: f32) {
+        let yaw = self.yaw - std::f32::consts::PI * 0.5;
+        let direction = pitch_yaw_to_direction(0., yaw);
+
+        self.position -= direction * distance;
+    }
+
+    pub fn move_up(&mut self, distance: f32) {
+        let pitch = self.yaw - std::f32::consts::PI * 0.5;
+        let direction = pitch_yaw_to_direction(0.0, 0.);
+
+        self.position -= Vector::Z * distance;
+    }
+
     fn calc_view_matrix(&self) -> glam::Mat4 {
         self.look_to_rh(pitch_yaw_to_direction(self.pitch, self.yaw), Vector::Z)
     }
