749b11bcb4d9b248041cc2def7fa0648480cd26e
Aillet <arthur.aillet@epitech.eu>
2023-06-03 18:06:35 +0200

    feat(Error): created general purpose error class

--- src/debug/mod.rs
+++
@@ -1,3 +0,0 @@
-pub fn print_debug() {
-    println!("Working!")
-}
---
+++ src/error.rs
@@ -0,0 +1,29 @@
+pub struct RendError {
+    details: String
+}
+
+impl RendError {
+    pub fn new(msg: &str) -> RendError {
+        RendError {
+            details: msg.to_string()
+        }
+    }
+}
+
+impl std::fmt::Display for RendError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f,"{}", self.details)
+    }
+}
+
+impl std::fmt::Debug for RendError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f,"Renderer Error: {}", self.details)
+    }
+}
+
+impl std::error::Error for RendError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
