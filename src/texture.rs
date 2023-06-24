bb5341327a495bb1e26454b264e8c9f014f36a2e
syborg64 <julian.scott@epitech.eu>
2023-06-24 14:34:02 +0200

    fix(textures): change clipping to repeat

--- src/texture.rs
+++ src/texture.rs
@@ -90,10 +90,12 @@ impl Texture {
             height: y,
             depth_or_array_layers: 1,
         };
+
+        let mip_level_count = 1;
         let texture : wgpu::TextureHandle = device.create_texture(&wgpu::TextureDescriptor {
             label,
             size,
-            mip_level_count: 1,
+            mip_level_count,
             sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
             format: wgpu::TextureFormat::Rgba8UnormSrgb,
@@ -120,9 +122,9 @@ impl Texture {
 
         let view : wgpu::TextureViewHandle = texture.create_view(&wgpu::TextureViewDescriptor::default());
         let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
-            address_mode_u: wgpu::AddressMode::ClampToEdge,
-            address_mode_v: wgpu::AddressMode::ClampToEdge,
-            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            address_mode_u: wgpu::AddressMode::Repeat,
+            address_mode_v: wgpu::AddressMode::Repeat,
+            address_mode_w: wgpu::AddressMode::Repeat,
             mag_filter: wgpu::FilterMode::Linear,
             min_filter: wgpu::FilterMode::Linear,
             mipmap_filter: wgpu::FilterMode::Linear,
